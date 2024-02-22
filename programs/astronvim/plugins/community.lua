3061bc8f49b78e5bebc0314ddf04ce3f7824bd76
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-02-21 22:00:35 -0500

    feat(neovim): add more language packs

--- programs/astronvim/plugins/community.lua
+++ programs/astronvim/plugins/community.lua
@@ -5,5 +5,11 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.nix" }
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 }
