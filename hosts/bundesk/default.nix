d32a89d4b7ef23fe34aab3325b5a2af86f4fae79
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-02-25 12:10:47 -0500

    fix: correct networking import path

--- hosts/bundesk/default.nix
+++ hosts/bundesk/default.nix
@@ -4,7 +4,7 @@
     ./hardware-configuration.nix
 
     # DNS and firewall. Majorly slows down laptop.
-    ./system/security/networking.nix
+    ../../system/security/networking.nix
   ];
 
   fileSystems = {
