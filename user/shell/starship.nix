f3ed224fae1cb9327e0bb1c7a428585886dde6db
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-02-26 20:40:16 -0500

    feat(starship): pure prompt

--- user/shell/starship.nix
+++ user/shell/starship.nix
@@ -1,112 +1,52 @@
-{
-  lib,
-  config,
-  ...
-}:
-with config.lib.stylix.colors; {
+{lib, ...}: {
   programs.starship = {
     enable = true;
     enableTransience = true;
     settings = {
       format = lib.concatStrings [
-        "[](${withHashtag.base08})"
-        "$os"
+        "$username"
         "$hostname"
-        "[](bg:${withHashtag.base09} fg:${withHashtag.base08})"
         "$directory"
-        "[](bg:${withHashtag.base0A} fg:${withHashtag.base09})"
         "$git_branch"
+        "$git_state"
         "$git_status"
-        "[](bg:${withHashtag.base0B} fg:${withHashtag.base0A})"
         "$cmd_duration"
-        "[](fg:${withHashtag.base0B})"
         "$line_break"
+        "$python"
         "$character"
       ];
-
-      hostname = {
-        ssh_symbol = " ";
-        format = "[ $hostname ]($style)";
-        style = "bg:${withHashtag.base08} fg:${withHashtag.base01}";
+      directory = {style = "blue";};
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
       };
-
-      directory = {
-        style = "fg:${withHashtag.base01} bg:${withHashtag.base09}";
-        fish_style_pwd_dir_length = 1;
-        truncation_length = 5;
-        format = "[ $path ]($style)";
-      };
-
-      directory.substitutions = {
-        "Documents" = "󰈙 ";
-        "Downloads" = " ";
-        "Music" = "󰝚 ";
-        "Pictures" = " ";
-      };
-
       git_branch = {
-        symbol = "󰘬";
-        style = "fg:${withHashtag.base01} bg:${withHashtag.base0A}";
-        format = "[[ $symbol $branch ](fg:${withHashtag.base01} bg:${withHashtag.base0A})]($style)";
+        format = "[$branch]($style)";
+        style = "bright-black";
       };
-
       git_status = {
-        style = "bg:${withHashtag.base0A}";
-        format = "[[($all_status$ahead_behind )](fg:${withHashtag.base01} bg:${withHashtag.base0A})]($style)";
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
       };
-
+      git_state = {
+        format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+        style = "bright-black";
+      };
       cmd_duration = {
-        style = "fg:${withHashtag.base01} bg:${withHashtag.base0B}";
-        format = "[ $duration]($style)";
+        format = "[$duration]($style) ";
+        style = "yellow";
       };
-
-      nix_shell = {symbol = " ";};
-
-      os = {
-        disabled = false;
-        style = "fg:${withHashtag.base01} bg:${withHashtag.base08}";
-      };
-
-      os.symbols = {
-        Alpaquita = " ";
-        Alpine = " ";
-        Amazon = " ";
-        Android = " ";
-        Arch = " ";
-        Artix = " ";
-        CentOS = " ";
-        Debian = " ";
-        DragonFly = " ";
-        Emscripten = " ";
-        EndeavourOS = " ";
-        Fedora = " ";
-        FreeBSD = " ";
-        Garuda = "󰛓 ";
-        Gentoo = " ";
-        HardenedBSD = "󰞌 ";
-        Illumos = "󰈸 ";
-        Linux = " ";
-        Mabox = " ";
-        Macos = " ";
-        Manjaro = " ";
-        Mariner = " ";
-        MidnightBSD = " ";
-        Mint = " ";
-        NetBSD = " ";
-        NixOS = " ";
-        OpenBSD = "󰈺 ";
-        openSUSE = " ";
-        OracleLinux = "󰌷 ";
-        Pop = " ";
-        Raspbian = " ";
-        Redhat = " ";
-        RedHatEnterprise = " ";
-        Redox = "󰀘 ";
-        Solus = "󰠳 ";
-        SUSE = " ";
-        Ubuntu = " ";
-        Unknown = " ";
-        Windows = "󰍲 ";
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
       };
     };
   };
