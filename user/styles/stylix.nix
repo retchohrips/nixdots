9477ef11d8c124609a0574550524f465e5924754
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-02-26 20:40:51 -0500

    style(stylix): reduce duplication

--- user/styles/stylix.nix
+++ user/styles/stylix.nix
@@ -1,42 +1,40 @@
 {
-  inputs,
-  config,
-  lib,
   pkgs,
   userSettings,
   ...
 }: {
-  stylix.image = ../../wallpapers/${userSettings.pape};
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${userSettings.theme}.yaml";
-  stylix.targets.rofi.enable = false;
-  stylix.targets.waybar.enable = false;
-  stylix.targets.fish.enable = false; # Fish can just use terminal colors
+  stylix = {
+    image = ../../wallpapers/${userSettings.pape};
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${userSettings.theme}.yaml";
+    targets.rofi.enable = false;
+    targets.waybar.enable = false;
+    targets.fish.enable = false; # Fish can just use terminal colors
 
-  stylix.cursor = {
-    size = 24;
-    package = pkgs.catppuccin-cursors.mochaDark;
-    name = "Catppuccin-Mocha-Dark-Cursors";
+    cursor = {
+      size = 24;
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "Catppuccin-Mocha-Dark-Cursors";
+    };
+
+    fonts = {
+      monospace = {
+        name = "${userSettings.font} Nerd Font";
+        package = with pkgs; (nerdfonts.override {fonts = ["${userSettings.font}"];});
+      };
+      sansSerif = {
+        name = "Inter";
+        package = pkgs.inter;
+      };
+      serif = {
+        name = "Noto Serif";
+        package = pkgs.noto-fonts;
+      };
+      emoji = {
+        name = "Noto Color Emoji";
+        package = pkgs.noto-fonts-emoji-blob-bin;
+      };
+    };
   };
-
-  stylix.fonts = {
-    monospace = {
-      name = "${userSettings.font} Nerd Font";
-      package = with pkgs; (nerdfonts.override {fonts = ["${userSettings.font}"];});
-    };
-    sansSerif = {
-      name = "Inter";
-      package = pkgs.inter;
-    };
-    serif = {
-      name = "Noto Serif";
-      package = pkgs.noto-fonts;
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      package = pkgs.noto-fonts-emoji-blob-bin;
-    };
-  };
-
   gtk.iconTheme = {
     name = "Papirus-Dark";
     package = pkgs.catppuccin-papirus-folders.override {
