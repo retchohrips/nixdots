a124ac88b442cf85afd6375ad4b9a562c6a7ed54
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-03-12 19:46:36 -0400

    add winetricks and gamescope

--- system/gaming.nix
+++ system/gaming.nix
@@ -5,14 +5,15 @@
 }: {
   imports = [
     inputs.nix-gaming.nixosModules.pipewireLowLatency
-    inputs.nix-gaming.nixosModules.steamCompat
   ];
+  environment.systemPackages = with pkgs; [winetricks gamescope];
   hardware.opengl.driSupport32Bit = true;
   # environment.systemPackages = with pkgs; [gamemode];
   # programs.gamemode.enable = true;
   programs.steam = {
     enable = true;
     extraCompatPackages = [inputs.nix-gaming.packages.${pkgs.system}.proton-ge];
+    gamescopeSession.enable = true;
   };
-  # services.pipewire.lowLatency = {enable = true;};
+  services.pipewire.lowLatency = {enable = true;};
 }
