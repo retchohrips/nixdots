ab78ac7900e6869e79da25e3d4a26b0b794603db
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-03-31 17:16:25 -0400

    feat(fish): do not enable coloring if kitty, remove git aliases for forgit

--- home/programs/terminal/shell/fish.nix
+++ home/programs/terminal/shell/fish.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
+  stylix.targets.fish.enable = !config.programs.kitty.enable;
   programs.fish = {
     enable = true;
     shellAliases = {
@@ -8,19 +13,11 @@
     };
     shellAbbrs = {
       g = "git";
-      ga = "git add";
       gaa = "git add --all";
-      gb = "git branch --verbose";
       gc = "git commit -m";
       gca = "git commit --amend";
       gcl = "git clone";
-      gs = "git status";
-      gss = "git status --short";
-      gd = "git difftool --no-symlinks --dir-diff";
-      gds = "git difftool --no-symlinks --dir-diff --staged";
       gf = "git fetch";
-      gi = "git init";
-      gl = "git log --oneline --decorate --graph -n 10";
       gm = "git merge";
       gp = "git push";
       gpu = "git pull";
