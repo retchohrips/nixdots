a5ced292581fb3c7724e03e46429a1d7c9038f76
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-04-03 21:48:45 -0400

    oops here's bluetooth

--- modules/system/hardware/bluetooth.nix
+++ modules/system/hardware/bluetooth.nix
@@ -3,7 +3,19 @@
     enable = true;
     package = pkgs.bluez5-experimental;
     powerOnBoot = true;
+    settings = {
+      General = {
+        JustWorksRepairing = "always";
+        MultiProfile = "multiple";
+        Experimental = true;
+      };
+    };
   };
+
+  boot.kernelParams = [
+    "bluetooth"
+    "btusb"
+  ];
 
   services.blueman.enable = true;
 }
