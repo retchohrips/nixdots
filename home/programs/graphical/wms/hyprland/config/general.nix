7aad1c572b31a3da818bfe668305b66273b005d2
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-04-06 18:43:08 -0400

    no borders .

--- home/programs/graphical/wms/hyprland/config/general.nix
+++ home/programs/graphical/wms/hyprland/config/general.nix
@@ -1,7 +1,7 @@
 {
   wayland.windowManager.hyprland.settings.general = {
     layout = "dwindle";
-    border_size = 2;
+    border_size = 0;
     gaps_in = 3;
     gaps_out = 6;
     allow_tearing = false;
