7e3ae140154ada4981f3bb7d32dd8f7f66f56704
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-04-08 09:10:40 -0400

    add laptop to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,16 @@
 let
   # users
-  bunny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKBT0vLxWU+iLJG3AzCDJcAXLrywF6dgnGDcwHmQwjs";
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKBT0vLxWU+iLJG3AzCDJcAXLrywF6dgnGDcwHmQwjs";
 
   # hosts
   bundesk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAebvgC9nLUMl+F2jf1pbwtXNchKI1nRsdy1yKXjRfDG";
+  pawpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg5trDBs0WEmy9nid9AfTrfIqeMl7turnSjqscIpSEi";
 
   # aliases
-  personal = [bundesk];
+  personal = [bundesk pawpad];
 
   # helpers
-  mkSecrets = list: list ++ [bunny];
+  mkSecrets = list: list ++ [user];
 in {
   "lastfm-account.age".publicKeys = mkSecrets personal;
   "wakatime.age".publicKeys = mkSecrets personal;
