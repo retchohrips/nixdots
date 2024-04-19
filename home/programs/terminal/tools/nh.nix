de14f5f75573ba3329de7258ada2fc5207c2fa4e
retchohrips <44993244+retchohrips@users.noreply.github.com>
2024-04-19 17:30:01 -0400

    add nh

---
+++ home/programs/terminal/tools/nh.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  userSettings,
+  ...
+}: {
+  programs.nh = {
+    # "yet-another-nix-helper"
+    enable = true;
+    package = pkgs.nh;
+    clean.enable = true;
+    flake = "/home/${userSettings.username}/.dotfiles";
+  };
+}
